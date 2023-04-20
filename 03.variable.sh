531b33ff23ee84e5637b15fa898623c6ace1f608
bkaki <bhaskarkakii77@gmail.com>
2023-04-20 14:25:16 +0530

    Adding Print statements

--- 03.variable.sh
+++ 03.variable.sh
@@ -1,14 +1,19 @@
 a=10
 name=DevOps
 
+b=20
+name=Bhaskar
+
 # Print Variable
-echo a = $a
+echo a b  = $a b
 echo name = ${name}
 
 #
 #DATE=2023-04-18
 DATE=$(date +%F)
 echo Today date is ${DATE}
+
+
 
 ARTH=$((2-3*4/2))
 
