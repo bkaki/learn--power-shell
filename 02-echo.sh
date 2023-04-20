c0a267cbafbce2aa4863f4160e9977c7a18b57ad
bkaki <bhaskarkakii77@gmail.com>
2023-04-20 14:19:31 +0530

    Adding Print statements

--- 02-echo.sh
+++ 02-echo.sh
@@ -6,7 +6,7 @@ echo Hello World
 # All the input should be there in quotes (Double quotes preferred)
 # \e - Enable a color
 # COLm - Color Code - Red(31m), Green(32m), Yellow(33m), Blue(34m), Magenta(35m), Cyan(36m)
-# \e[0m - Disable the color. (Once we enable the color we have to diable that, else color folloows to next lines)
+# \e[0m - Disable the color. (Once we enable the color we have to diable that, else color follows to next lines)
 
 echo -e "\e[31mRED COLOR\e[0m"
 echo Normal Text
