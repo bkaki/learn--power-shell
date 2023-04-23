6a5058a1708ea4dd719b335f82be1f80a4b8e8fa
bkaki <bhaskarkakii77@gmail.com>
2023-04-23 17:37:47 +0530

    Adding Print statements

--- 05-if.sh
+++ 05-if.sh
@@ -1,10 +1,6 @@
 fruit_name=$1
 quantity=$2
 
-if [ -z "$fruit_name" ]; then
-  echo Input Missing
-  exit
-fi
 
 if [ "$fruit_name" == "mango" ]
 then
@@ -13,10 +9,3 @@ else
   echo Fruit does not exist
 fi
 
-## Always double quote variables in expressions
-
-if [ "$quantity" -gt 100 ]; then
-  echo Mango Price - 0.5$
-else
-  echo Mango Price - 1$
-fi
