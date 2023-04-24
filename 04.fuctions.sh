d27f1765d8ce400d18e65b588ee8b38b2869b68f
bkaki <bhaskarkakii77@gmail.com>
2023-04-24 11:01:43 +0530

    Adding Print statements

--- 04.fuctions.sh
+++ 04.fuctions.sh
@@ -9,4 +9,13 @@ function_name() {
 # Call a function
 function_name
 
+### We can send inputs to the function and we can access them with special variables $1-$n, $*, $#
+
+fucntion_name() {
+
+    echo First Argument = $1
+    echo Second Argument = $2
+    echo All Arguments = $*
+    echo No Of Arguments = $#
+  }
 
