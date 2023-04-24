c354f3fbc92b9169dd65b087e72708d647f7f741
bkaki <bhaskarkakii77@gmail.com>
2023-04-25 04:01:17 +0530

    Adding Print statements

--- 09.fucntion.sh
+++ 09.fucntion.sh
@@ -1,9 +1,9 @@
  func_status_check() {
- if [ $2 -eq 0 ]; then
+ if [ $1 -eq 0 ]; then
     echo -e "\e[32mSUCCESS\e[0m"
   else
     echo -e "\e[31mFAILURE\e[0m"
     exit
   fi
   }
-  func_status_check
+  func_status_check $?
