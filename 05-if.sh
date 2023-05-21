e76b4219f49df3c794ec8eed24ca9c0468c60673
bkaki <bhaskarkakii77@gmail.com>
2023-05-21 18:02:01 +0530

    terraform

--- 05-if.sh
+++ 05-if.sh
@@ -1,25 +1,25 @@
-fruit_name=mango
-quantity=10
+fruit_name=$1
+quantity=$2
+
+
+if [ -z "$fruit_name" ]; then
+  echo Input Missing
+ exit
+fi
 
 if [ "$fruit_name" == "mango" ]
 then
   echo Mango quantity - $quantity
+
+else
+  echo Fruit does not exist
 fi
 
-#if [ -z "$fruit_name" ]; then
-#  echo Input Missing
-# exit
-#fi
 
 
-#else
-#  echo Fruit does not exist
-#fi
-
-
-#if [ "$quantity" -gt 100 ]; then
-#  echo mango price - 0.5$
-#else
-#  echo mango price -1$
-#fi
+if [ "$quantity" -gt 100 ]; then
+  echo mango price - 0.5$
+else
+  echo mango price -1$
+fi
 
