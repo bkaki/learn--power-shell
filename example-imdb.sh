84dee9b6998fb52bf06d42cf41fd94b71ff91075
bkaki <bhaskarkakii77@gmail.com>
2023-05-23 02:20:51 +0530

    terraform

--- example-imdb.sh
+++ example-imdb.sh
@@ -2,4 +2,17 @@
 # > 7 - Good
 # < 7 - Average
 
-input=603692
+input=$1
+
+
+if [ -z "$input"]; then
+  echo input movie name missing
+fi
+
+persent=$(curl -s  https://www.themoviedb.org/movie/603692-john-wick-chapter-4 | grep user_score_chart | xargs -n1 | grep data-percent | awk -F = '{print $2}' | awk -F . '{print $1}')
+
+if [ "$percent" -ge 70 ]; then
+  echo movie is good
+  else
+  echo movie is bad
+fi
